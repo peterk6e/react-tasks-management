fd3628f427e165387b7d3bc7e567f8c712abde40
pierrek6e <pierrecass75@gmail.com>
2022-05-26 22:23:18 +0700

    add task state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import {FC} from 'react';
-import { InputField } from './components/InputField';
-import './App.css';
+import { FC, useState } from "react";
+import { InputField } from "./components/InputField";
+import "./App.css";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+
   return (
     <div className="App">
       <span className="heading">Tasks Management Tool</span>
-      <InputField />
+      <InputField task={task} setTask={setTask}/>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,18 @@
 import { FC } from "react";
 import "./styles.css";
 
-export const InputField: FC = () => {
+interface Props {
+  task: string;
+  setTask(task: string): void; //React.Dispatch<React.SetStateAction<string>>
+}
+
+export const InputField = ({ task, setTask }: Props) => {
   return (
     <form className="input">
       <input
         type="input"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
         placeholder="Enter a Task..."
         className="input__box"
       />
