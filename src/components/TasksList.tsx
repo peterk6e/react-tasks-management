9522252c5446732f9f3d5cd71773fa28758a75a8
pierrek6e <pierrecass75@gmail.com>
2022-05-27 22:19:18 +0700

    prepare UI with container

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -4,15 +4,45 @@ import "./styles.css";
 
 interface Props {
   tasks: Task[];
-  setTasks: React.Dispatch<React.SetStateAction<Task[]>>
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
 }
 
 export const TasksList = ({ tasks, setTasks }: Props) => {
   return (
-    <div className="tasks">
-      {tasks.map((task: Task, key: number) => {
-        return <SingleTask task={task} tasks={tasks} key={key} setTasks={setTasks}/>
-      })}
+    <div className="container">
+      <div className="backlog tasks">
+        <span className="tasks-heading">Backlog</span>
+
+        {tasks.map((task: Task, key: number) => {
+          return (
+            <SingleTask
+              task={task}
+              tasks={tasks}
+              key={key}
+              setTasks={setTasks}
+            />
+          );
+        })}
+      </div>
+      <div className="inprogress tasks">
+        <span className="tasks-heading">In progress</span>
+        {tasks.map((task: Task, key: number) => {
+          return (
+            <SingleTask
+              task={task}
+              tasks={tasks}
+              key={key}
+              setTasks={setTasks}
+            />
+          );
+        })}
+      </div>
+      <div className="intest tasks">
+        <span className="tasks-heading">Test</span>
+      </div>
+      <div className="completed tasks">
+        <span className="tasks-heading">Completed</span>
+      </div>
     </div>
   );
 };
